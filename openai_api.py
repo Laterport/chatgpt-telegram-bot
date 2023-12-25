4877025dc937f204d06a169231063e4760e604fd
Nick <lossless.senator@gmail.com>
2023-12-25 06:03:03 +0300

    stream=True

--- openai_api.py
+++ openai_api.py
@@ -5,14 +5,14 @@ from retry import retry
 from usage import UsageStats
 
 class ChatGPT:
-    ENDPOINT = "https://neuroapi.host/v1"  # Обновите с вашим адресом сервера
+    ENDPOINT = "https://neuroapi.host/v1"  # Update with your server address
     MODEL_NAME = "gpt-4-1106-preview"
-    BEHAVIOUR = {"role": "system", "content": "You are a helpful assistant"}
+    BEHAVIOUR = [{"role": "system", "content": "You are a helpful assistant"}]
 
     def __init__(self, api_key):
         self.api_key = api_key
         self.__usage = UsageStats()
-        self.logger = logging.getLogger(__name__)  # Создаем логгер
+        self.logger = logging.getLogger(__name__)  # Create a logger
 
     @retry(tries=3, delay=3, backoff=1, max_delay=3)
     def get_response(self, user_id, chat_messages, update_usage=True):
@@ -23,27 +23,34 @@ class ChatGPT:
             }
             payload = {
                 "model": self.MODEL_NAME,
-                "messages": [self.BEHAVIOUR] + chat_messages
+                "messages": self.BEHAVIOUR + chat_messages
             }
 
-            # Обновленная строка URL с /gpt-3.5-turbo/chat/completions
+            # Update the URL with /chat/completions
             url = f"{self.ENDPOINT}/chat/completions"
 
             self.logger.info(f"Sending request to {url} with payload: {payload}")
 
-            response = requests.post(url, headers=headers, data=json.dumps(payload))
+            response = requests.post(url, headers=headers, data=json.dumps(payload), stream=True)
 
-            self.logger.info(f"Received response with status code: {response.status_code}, content: {response.content}")
+            self.logger.info(f"Received response with status code: {response.status_code}")
 
             if response.status_code != 200:
                 raise Exception(f"Request failed with status code: {response.status_code}")
 
-            response_data = response.json()
+            for chunk in response.iter_content(chunk_size=1024):
+                if chunk:
+                    # Process the chunk as needed
+                    print(chunk.decode("utf-8"))
+
+            response_data = json.loads(response.content)
             if update_usage:
                 self.__usage.update(user_id, response_data.get("usage", {}).get("total_tokens", 0))
+
             return response_data["choices"][0]["message"]["content"]
         except Exception as e:
             self.logger.error(f"Error during request: {e}")
             raise e
-# Настроим логирование
-logging.basicConfig(level=logging.INFO)  # Уровень логирования INFO
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)  # Set log level to INFO
